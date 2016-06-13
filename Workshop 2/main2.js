f1085725889e071121af213b3641110791fd9444
Igelex <sergatam@yandex.com>
2016-06-13 18:19:30 +0200

    Kleiner Fehler behoben

--- Workshop 2/main2.js
+++ Workshop 2/main2.js
@@ -74,21 +74,16 @@ app.delete('/users', function(req, res) {
       var i = 1;
       for (i in rep){
         client.del('user:i', function(err, rep){
-  /*        if (rep == 1) {
+         if (rep == 1) {
             res.status(200).type('text').send('User '+ i + ' gelöscht');
           }
           else {
             res.status(404).type('text').send('Der User ' + i + ' existiert nicht');
-        }*/
+        }
       });
     }
   });
 });
-
-
-
-
-
 
 app.delete('/users/:id', function(req, res){
     client.del('user:'+req.params.id, function(err, rep) {
@@ -101,122 +96,24 @@ app.delete('/users/:id', function(req, res){
     });
 });
 
-app.put('/users/:id', jsonParser, function(req, res){
-            var neu = req.body;
-            neu.id = req.params.id;
-
-            client.set('user:'+req.params.id, JSON.stringify(neu),  function(err, rep){
-                res.status(200).type('json').send(neu);
-            });
-
-});
-//########################################################################USERS
 
 
-//FAHRT########################################################################
 
 
-app.post('/fahrten',function(req, res){
-    var newFahrt = req.body;
-    client.incr('id:fahrten',function(err,rep){
-        newFahrt.id = rep;
-        client.set('fahrt:'+newFahrt.id, JSON.stringify(newFahrt),function(err,rep){
-                    res.json(newFahrt);
-                });
-        });
-});
 
 
-app.delete('/fahrt/:id', function(req, res){
-    client.del('fahrt:'+req.params.id, function(err, rep) {
-        if (rep == 1) {
-            res.status(200).type('text').send('Die Fahrt '+ req.params.id + ' gelöscht');
-        }
-        else {
-            res.status(404).type('text').send('Die Fahrt' + req.params.id + 'existiert nicht');
-        }
-    });
-});
 
 
-app.get('/fahrten', function (req, res) {
-   client.keys('fahrt:*', function(err,rep){
-       var fahrten = [];
-       if(rep.length == 0){
-       res.json(fahrten);
-       return;
-   }
-       client.mget(rep,function(err,rep){
-       rep.forEach(function(val){
-           fahrten.push(JSON.parse(val));
-       });
- fahrten = fahrten.map(function(fahrten){
-        return {Start: fahrten.start, Ziel : fahrten.ziel, Platze : fahrten.plaetze,
-                ID: fahrten.id};
-        });
-      res.json(fahrten);
-    });
-  });
-});
 
-app.get('/fahrten/:id', function(req, res){
-    client.get('fahrt:'+req.params.id, function(err, rep){
-        if (rep) {
-            res.type('json').send(rep);
-        }
-        else{
-            res.status(404).type('text').send('Fahrt '+ req.params.id +
-                                        ' existiert nicht');
-        }
-    });
-});
 
-//nach Start ausgeben
-/*app.get('/fahrten', function(req, res) {
-  client.keys('fahrt:*', function(err, rep){
-    console.dir(rep);
-    if (rep.length == 0) {
-      res.json([]);
-      return;
-    }
-    client.mget(rep, function(err, rep){
-      var fahrten = rep.map(function(userStringified){
-        var fahrten = JSON.parse(userStringified);
-        if (req.query.start !== undefined) {
-          res.json(fahrten.filter(function(e, i, arr){
-            return e.start == req.query.start
 
-        }));
-      }
-      else{
-        res.json(fahrten);
-      }
-      });
-    });
-  });
-});*/
 
-app.delete('/fahrten/:id', function(req, res){
 
-    client.del('fahrt:'+req.params.id, function(err, rep) {
-        if (rep == 1) {
-            res.status(200).type('text').send('Fahrt '+ req.params.id + ' gelöscht');
-        }
-        else {
-            res.status(404).type('text').send('Fahrt ' + req.params.id + ' existiert nicht');
-        }
-    });
-});
 
-app.put('/fahrten/:id', jsonParser, function(req, res){
-            var newFahrt = req.body;
-            newFahrt.id = req.params.id;
 
-            client.set('fahrt:'+req.params.id, JSON.stringify(newFahrt),  function(err, rep){
-                res.status(200).type('json').send(newFahrt);
-            });
 
-});
+
+
 
 //FAHRT########################################################################
 
